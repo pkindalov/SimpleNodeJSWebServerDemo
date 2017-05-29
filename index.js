8fa290d8112c0e93f3e3148f6626fcde84d86d31
pkindalov <panayotkindalov@gmail.com>
2017-05-29 12:05:43 +0300

    Update index.js in main directory

--- index.js
+++ index.js
@@ -1,76 +1,27 @@
 const http = require('http');
 const url = require('url');
-const fs = require('fs');
 const port = 1202;
+
+
+const homePageHandler = require('./handlers/home-page');
+const faviconHandler = require('./handlers/favicon');
+const staticFilesHandler = require('./handlers/static-files');
 
 http
     .createServer((req, res) => {
         req.path = url.parse(req.url).pathname;
 
-        if(path === '/'){
-            // fs.readFile('./index.html', (err, data) => {
-            //     if(err){
-            //         console.log(err);
-            //         return;
-            //     }
-
-            //     res.writeHead(200, {
-            //         'Content-type': 'text/html'
-            //     });
-
-            //     res.write(data);
-            //     res.end();
-
-            // });
-        } else if(path === favicon){
-
-            fs.readFile('.' + favicon, (err, data) => {
-
-                if(err){
-                    console.log(err);
-                    return;
-                }
-
-                res.writeHead(200, {
-                    'Content-type': 'image/x-icon'
-                });
-
-                res.write(data);
-                res.end();
-
-            });
+        let handlers = [
+            homePageHandler,
+            faviconHandler,
+            staticFilesHandler
+        ];
 
 
-        }else{
-
-            fs.readFile('.' + path, (err, data) => {
-                if(err){
-                    res.writeHead(404, {
-                        'Content-type':'text/plain'
-                    });
-
-                    res.write('Resource not found');
-                    res.end();
-                }
-
-
-                let contentType = 'text/plain';
-                if(path.endsWith('.css')){
-                    contentType = 'text/css';
-                }else if(path.endsWith('.js')){
-                    contentType = 'application/javascript';
-                }
-
-
-                res.writeHead(200, {
-                    'Content-type': contentType
-                });
-                res.write(data);
-                res.end();
-
-            });
-
+        for(let handler of handlers){
+            
         }
+
     
     })
         .listen(port);
