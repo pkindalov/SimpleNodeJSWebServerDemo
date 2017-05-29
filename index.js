d474e24fa9522b80e2c80c8787bc5fad1e877448
pkindalov <panayotkindalov@gmail.com>
2017-05-29 12:41:38 +0300

    Little changes

--- index.js
+++ index.js
@@ -2,22 +2,13 @@ const http = require('http');
 const url = require('url');
 const port = 1203;
 
+const handlers = require('./handlers');
 
-const homePageHandler = require('./handlers/home-page');
-const faviconHandler = require('./handlers/favicon');
-const staticFilesHandler = require('./handlers/static-files');
 
 http
     .createServer((req, res) => {
         req.path = url.parse(req.url).pathname;
-
-        let handlers = [
-            homePageHandler,
-            faviconHandler,
-            staticFilesHandler
-        ];
-
-
+        
         for(let handler of handlers){
             let next = handler(req, res);
             if(!next){
